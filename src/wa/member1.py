7a1423a735ec25f0691dda4f839ffe4c7f84a5bc
Russell <34661910+russelloverton@users.noreply.github.com>
2025-11-08 13:10:59 -0600

    Update member1.py

--- src/wa/member1.py
+++ src/wa/member1.py
@@ -1,12 +1,32 @@
+from datetime import datetime, date
 
+TASKLIST = []
 
 class Task:
-    def __init__(self, name, due, work_date, bin):
+    def __init__(self, name, due, work_date, cat):
         self.name = name
         self.due = due
         self.work_date = work_date
-        self.bin = bin
+        self.cat = cat
 
+def make_task(name, due, work_date, cat):
+    task = Task(name, due, work_date, cat)
+    return task
 
-def make_task(name, due, work_date, bin):
-    return Task(name, due, work_date, bin)
+def get_task_name(task):
+    return task.name
+
+def get_task_due(task):
+    return task.due
+
+def get_task_work_date(task):
+    return task.work_date
+
+def get_task_cat(task):
+    return task.cat
+
+def get_task_all(task):
+    return task
+
+def add_to_tasklist(tasklist, task):
+    tasklist.append(task)
