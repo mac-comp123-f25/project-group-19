8356a815244281b8516ff1365a860c76c8bba31b
Russell <34661910+russelloverton@users.noreply.github.com>
2025-11-12 12:59:44 -0600

    added creation of tasks

--- src/wa/member1.py
+++ src/wa/member1.py
@@ -1,16 +1,16 @@
+import tkinter as tk
+from tkinter import simpledialog
 
 TASKLIST = []
 
 class Task:
-    def __init__(self, name, due, work_date, cat):
+    def __init__(self, name, due):
         self.name = name
         self.due = due
-        self.work_date = work_date
-        self.cat = cat
 
-def make_task(name, due, work_date, cat):
-    task = Task(name, due, work_date, cat)
-    return task
+def make_task(name, due):
+    task = Task(name, due)
+    add_to_tasklist(TASKLIST, task)
 
 def get_task_name(task):
     return task.name
@@ -21,7 +21,7 @@ def get_task_due(task):
 def get_task_work_date(task):
     return task.work_date
 
-#category
+# category
 def get_task_cat(task):
     return task.cat
 
@@ -30,3 +30,33 @@ def get_task_all(task):
 
 def add_to_tasklist(tasklist, task):
     tasklist.append(task)
+
+
+# ----- GUI class and methods -----
+class BasicGui:
+    def __init__(self):
+        self.mainWin = tk.Tk()
+        self.mainWin.title("Planner")
+        self.input1 = tk.Entry(self.mainWin)
+        self.input1.grid(row=0, column=0)
+        self.input1.bind('<Return>', self.entry_response)
+        self.input1.bind('<Tab>', self.entry_response)
+        self.add_button = tk.Button(self.mainWin, text="Add Task", command=self.add_response)
+        self.add_button.grid(row=0, column=1)
+
+    def run(self):
+        self.mainWin.mainloop()
+
+    def entry_response(self, event):
+        text = self.input1.get()
+        self.input1.delete(0, tk.END)
+
+    def add_response(selfs):
+        task_name = simpledialog.askstring("Add Task", "Enter Task Name:")
+        due_date = simpledialog.askstring("Add Task", "Enter Due Date:")
+        make_task(task_name, due_date)
+        print(TASKLIST)
+
+# ----- Main program -----
+myGui = BasicGui()
+myGui.run()
