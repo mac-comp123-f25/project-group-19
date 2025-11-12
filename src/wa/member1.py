19277a75dde21a60de5d60f4e622fcf9ef9c1e49
Russell <34661910+russelloverton@users.noreply.github.com>
2025-11-12 15:50:37 -0600

    dynamic adding of tasks

--- src/wa/member1.py
+++ src/wa/member1.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 from tkinter import simpledialog
 
-TASKLIST = []
+tasklist = []
 
 class Task:
     def __init__(self, name, due):
@@ -10,27 +10,7 @@ class Task:
 
 def make_task(name, due):
     task = Task(name, due)
-    add_to_tasklist(TASKLIST, task)
-
-def get_task_name(task):
-    return task.name
-
-def get_task_due(task):
-    return task.due
-
-def get_task_work_date(task):
-    return task.work_date
-
-# category
-def get_task_cat(task):
-    return task.cat
-
-def get_task_all(task):
-    return task
-
-def add_to_tasklist(tasklist, task):
     tasklist.append(task)
-
 
 # ----- GUI class and methods -----
 class BasicGui:
@@ -43,6 +23,7 @@ class BasicGui:
         self.input1.bind('<Tab>', self.entry_response)
         self.add_button = tk.Button(self.mainWin, text="Add Task", command=self.add_response)
         self.add_button.grid(row=0, column=1)
+        self.current_row = 1
 
     def run(self):
         self.mainWin.mainloop()
@@ -51,12 +32,20 @@ class BasicGui:
         text = self.input1.get()
         self.input1.delete(0, tk.END)
 
-    def add_response(selfs):
+    def add_response(self):
         task_name = simpledialog.askstring("Add Task", "Enter Task Name:")
-        due_date = simpledialog.askstring("Add Task", "Enter Due Date:")
-        make_task(task_name, due_date)
-        print(TASKLIST)
+        if task_name:
+            due_date = simpledialog.askstring("Add Task", "Enter Due Date:")
+            if due_date:
+                make_task(task_name, due_date)
+                self.name = tk.Label(self.mainWin, text=task_name)
+                self.due_date = tk.Label(self.mainWin, text=due_date)
+                self.name.grid(row=self.current_row, column=1)
+                self.due_date.grid(row=self.current_row, column=2)
+                self.current_row += 1
+
 
 # ----- Main program -----
 myGui = BasicGui()
 myGui.run()
+
