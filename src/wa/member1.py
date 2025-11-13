74343e77c44a2db58bb79117e3c42c0a8dcb3481
Russell <34661910+russelloverton@users.noreply.github.com>
2025-11-12 19:06:54 -0600

    honestly couldn't tell you what I changed here, I think it now auto-puts the tasks on the gui though

--- src/wa/member1.py
+++ src/wa/member1.py
@@ -17,20 +17,15 @@ class BasicGui:
     def __init__(self):
         self.mainWin = tk.Tk()
         self.mainWin.title("Planner")
-        self.input1 = tk.Entry(self.mainWin)
-        self.input1.grid(row=0, column=0)
-        self.input1.bind('<Return>', self.entry_response)
-        self.input1.bind('<Tab>', self.entry_response)
         self.add_button = tk.Button(self.mainWin, text="Add Task", command=self.add_response)
-        self.add_button.grid(row=0, column=1)
+        self.add_button.grid(row=0, column=0)
         self.current_row = 1
+        self.mainWin.columnconfigure(1, pad=50)
+        self.mainWin.columnconfigure(2, pad=50)
+        self.tasks = []
 
     def run(self):
         self.mainWin.mainloop()
-
-    def entry_response(self, event):
-        text = self.input1.get()
-        self.input1.delete(0, tk.END)
 
     def add_response(self):
         task_name = simpledialog.askstring("Add Task", "Enter Task Name:")
@@ -42,7 +37,13 @@ class BasicGui:
                 self.due_date = tk.Label(self.mainWin, text=due_date)
                 self.name.grid(row=self.current_row, column=1)
                 self.due_date.grid(row=self.current_row, column=2)
+                self.checkbox = tk.Checkbutton(self.mainWin, text="Complete?")
+                self.checkbox.grid(row=self.current_row, column=3, command=self.remove_task)
                 self.current_row += 1
+                temp_dict = {self.name.}
+
+    def remove_task(self):
+
 
 
 # ----- Main program -----
