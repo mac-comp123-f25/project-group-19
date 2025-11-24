dba2a656f41f512f4ff505c03735ffdc599abfcb
HKLevy <hlevy@macalester.edu>
2025-11-24 10:41:40 -0600

    created csv to save tasks

--- src/wa/member3.py
+++ src/wa/member3.py
@@ -1,1 +1,8 @@
 """This is where Helen will try to figure out how to save stuff."""
+import csv
+#I think this works?
+def save_tasks(task_list):
+    with open('tasks.csv','w',newline='') as csvfile:
+        writer=csv.writer(csvfile)
+        for task in task_list:
+            writer.writerow([task[0],task[1]])
