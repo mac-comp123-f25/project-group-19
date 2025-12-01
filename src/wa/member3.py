f85a98f5d859cd0819d5030a644eb0a3f918c98b
HKLevy <hlevy@macalester.edu>
2025-12-01 10:08:30 -0600

    Update member3.py

--- src/wa/member3.py
+++ src/wa/member3.py
@@ -2,7 +2,7 @@
 import csv
 #I think this works?
 def save_tasks(task_list):
-    with open('tasks.csv','w',newline='') as csvfile:
-        writer=csv.writer(csvfile)
+    with open('tasks.csv','w',newline='') as my_csv:
+        writer=csv.writer(my_csv)
         for task in task_list:
             writer.writerow([task[0],task[1]])
